bd108c5fde4cf5600f7e50056d9bdecbbd7d66a9
Thiago Dal Santo <thiagodalsantodaluz@gmail.com>
2026-06-24 09:41:31 +0100

    CHORE: Github Actions workflows configuration

--- vite.config.ts
+++ vite.config.ts
@@ -206,6 +206,7 @@ function vitePluginStorageProxy(): Plugin {
 const plugins = [react(), tailwindcss(), jsxLocPlugin(), vitePluginManusRuntime(), vitePluginManusDebugCollector(), vitePluginStorageProxy()];
 
 export default defineConfig({
+  base: process.env.GITHUB_PAGES ? '/bricsinsights/' : '/',
   plugins,
   resolve: {
     alias: {
